8e78b934fb0487d5637c0452a9c73fe066246a0b
Raimund Kraemer <kraemer_raimund@web.de>
2024-10-07 17:58:32 +0200

    Add sample Kotlin project

---
+++ IntelliJ_Kotlin/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.2")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_22)
+    }
+}
