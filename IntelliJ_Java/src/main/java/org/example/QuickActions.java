9a8767c8fb9a082a65f719fdb835e4a925e25a8e
Raimund Kraemer <33892834+kraemer-raimund@users.noreply.github.com>
2024-10-30 22:05:00 +0100

    Add first few exercises for QuickActions in Java
    Copied and adapted from the Kotlin version.

--- IntelliJ_Java/src/main/java/org/example/QuickActions.java
+++ IntelliJ_Java/src/main/java/org/example/QuickActions.java
@@ -1,4 +1,19 @@
 package org.example;
 
+// Replace me with Javadoc.
+// To access the context actions, press "Alt + Enter" or "Option + Enter".
 public class QuickActions {
+
+    // Replace me with a ...
+    // ... block comment.
+
+    public void example() {
+
+        // Replace with guard clause (early return).
+        if (4 < 7) {
+            // Flip the operands.
+            // (Tip: Type "flip" while the list of context actions is open.)
+            var flip = 42 + 1337;
+        }
+    }
 }
