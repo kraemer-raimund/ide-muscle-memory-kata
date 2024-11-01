52840731dddb5202d0dfa9b856084e813592f996
Raimund Kraemer <33892834+kraemer-raimund@users.noreply.github.com>
2024-11-01 13:54:28 +0100

    Multiple cursors

--- IntelliJ_Java/src/main/java/org/example/Refactorings.java
+++ IntelliJ_Java/src/main/java/org/example/Refactorings.java
@@ -3,7 +3,15 @@ OPTIONAL CHALLENGE: Keyboard only!
  */
 package org.example;
 
+import java.time.DayOfWeek;
+import java.util.List;
+
 public class Refactorings {
+
+    // Remove the "List" suffixes (numbers, names, etc.) all at once using multiple cursors.
+    final List<Integer> numbersList = List.of(1, 2, 3);
+    final List<String> namesList = List.of("Jack", "John", "Jim", "Joe");
+    final List<DayOfWeek> weekendDaysList = List.of(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY);
 
     public static void examples() {
         // Rename.
